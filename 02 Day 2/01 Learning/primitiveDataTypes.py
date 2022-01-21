8a0a2f8041153bd2e4b17c92003107cda13a7eb7
Jaime Colman <mjackerjaime@gmail.com>
2022-01-21 18:30:18 -0300

    Added exercises one and two. Day 2

---
+++ 02 Day 2/01 Learning/primitiveDataTypes.py
@@ -0,0 +1,17 @@
+## Data Types:
+# String
+print("abcdef"[3])
+
+## Concatenation
+print("123" + "456")
+
+# Integer
+print(123 + 456)
+print(123_456)
+
+# Float
+print(12.3 + 4.56)
+
+# Boolean
+print(True)
+print(False)
---
+++ 02 Day 2/02 Exercises/01_Add_two_digits.py
@@ -0,0 +1,2 @@
+number = input("Enter two digits number: ")
+print("The sume of those two digits are: ", int(number[0]) + int(number[1]))
